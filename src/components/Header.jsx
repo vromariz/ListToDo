9f25e7faabd50121d6511bf53f8a6b3d7979cb95
vinicius <viniciusrfscabral@gmail.com>
2025-01-18 11:14:45 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+
+import Logo from '../img/Logo.png';
+
+console.log(Logo);
+
+
+export function Header() {
+    return(
+        <Header className = {styles.header}>
+            <img src = {Logo} alt= "logo do todo-list" />
+        </Header>
+    );
+}
